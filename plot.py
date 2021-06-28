9dc6b210ef7144a9cd298ca80f4455e32c3f4507
xoxor <napolitanobeatrice@gmail.com>
2021-06-28 15:55:25 +0200

    fixed Sec 2.2

--- plot.py
+++ plot.py
@@ -2,46 +2,69 @@ import sys, os, statistics, math, csv
 
 
 def main():
-    plotDistributionOriginalVector(10675,5)     #data of the primaire 2017
-    #n = 60%-40% , final k = 10% of 10000
-    plotPercentageByK(6405,4270,1000)
+    #Figure1: 
+    # worst case scenario: 51%-49% alpha beta, plot in function of size of x, n= 10.003 
+    plotByX(5103,4900,100, 10001,"51-49")
 
-def plotDistributionOriginalVector (n,k):
-    f = open('./data/distributionVector.csv', 'w')
+    #Figure2: 
+    # 60%-40% alpha beta, plot in function of size of x, n= 10.003 
+    plotByX(6003,4000,2, 1001,"60-40")
+    plotByX(7003,3000,2, 1001,"70-30")
+    plotByX(8003,2000,2, 1001,"80-20")
+    plotByX(9003,1000,2, 1001,"90-10")
+
+    #Figure3: 
+    # original scenario in the worst case 51%-49% alpha beta, x= 4449 , n= 10675 
+    plotDistributionOriginalVector(10675,4449)     #data of the primaire 2017
+
+def plotByX (alpha,beta,step,stop,name):
+    path = "./data/"+ name + "-"+ str(step)+ ".csv"
+    f = open(path, 'w')
+    writer = csv.writer(f)
+    header = ["x","ProbOfMiss"]
+    writer.writerow(header)
+    x=1
+    while x <= stop:
+        p = probability (alpha,beta,x)*100
+        writer.writerow([x,p])
+        print (x)
+        x=x+step
+    f.close
+
+def plotDistributionOriginalVector (n,x):
+    f = open('./data/original.csv', 'w')
     writer = csv.writer(f)
     header = ["BetterThanMed","WorseThanMed","Percentage","ProbOfMiss"]
     writer.writerow(header)
-    n1=int((n+1)/2)
-    n2=int((n-1)/2)
-    while (n1<=n and n2>=0):
-        p = round(probability (n1,n2,k)*100,2)
-        s = str(round(n1/n*100,2)) + "% - " + str(round(n2/n*100,2))+ "%"
-        writer.writerow([n1,n2,s,p])
-        n1=n1+1
-        n2=n2-1
+    alpha = findPercentage(n,51)
+    beta = n-alpha
+    p = probability (alpha,beta,x)*100
+    s = str(round(alpha/n*100,2)) + "% - " + str(round(beta/n*100,2))+ "%"
+    writer.writerow([alpha,beta,s,p])
+    for i in range(60,100,10):
+        alpha = findPercentage(n,i)
+        beta = n-alpha
+        p = probability (alpha,beta,x)*100
+        s = str(round(alpha/n*100,2)) + "% - " + str(round(beta/n*100,2))+ "%"
+        writer.writerow([alpha,beta,s,p])
+        print(i)
     f.close
 
-def plotPercentageByK (n1,n2,stop):
-    f = open('./data/changingK_n60-40.csv', 'w')
-    writer = csv.writer(f)
-    header = ["k","ProbOfMiss"]
-    writer.writerow(header)
-    k=0
-    p = round(probability (n1,n2,k)*100,2)
-    writer.writerow([k,p])
-    k=1
-    while k <= stop:
-        p = round(probability (n1,n2,k)*100,2)
-        writer.writerow([k,p])
-        k=k+2
-    f.close
+def probability (alpha,beta,x):
+    sum = 0
+    total = math.comb(alpha+beta,x)
+    p1= int((x-1)/2)
+    p2= int((x+1)/2)
+    while (p2<=x and p1>=0):
+        betterHalf = math.comb(alpha,p1)  #math.comb((n+1)/2,(x-1)/2)
+        worseHalf = math.comb(beta,p2)
+        sum = sum + ((betterHalf*worseHalf))
+        p1=p1-1
+        p2=p2+1
+    return sum/total
 
-
-def probability (n1,n2,k):
-    betterHalf = math.comb(n1,int((k-1)/2))  #math.comb((n+1)/2,(k-1)/2)
-    worseHalf = math.comb(n2,int((k+1)/2))
-    total = math.comb(n1+n2,k)
-    return (betterHalf*worseHalf)/total
+def findPercentage (n,p):
+    return math.ceil((p * n)/100)
 
 if __name__ == "__main__":
     main()
